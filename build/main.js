8e2e79e275e73e5a1300c521ecfce333448343cb
michije <78039822+michije@users.noreply.github.com>
2025-04-08 09:27:35 +0200

    Update main.js

--- build/main.js
+++ build/main.js
@@ -151,7 +151,7 @@ class Nomosenergy extends utils.Adapter {
     const authString = Buffer.from(`${this.nomosConfig.client_id}:${this.nomosConfig.client_secret}`).toString("base64");
     const headers = {
       Authorization: `Basic ${authString}`,
-      "Content-Type": "application/json"
+      "Content-Type": "application/x-www-form-urlencoded"
     };
     const data = "grant_type=client_credentials";
     try {
